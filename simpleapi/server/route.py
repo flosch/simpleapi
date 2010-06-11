919042fbfbc79c96b5c3cff118cfa1063f01e9e6
Florian Schlachter <flori@n-schlachter.de>
2010-06-11 21:16:11 +0200

    Standalone-server fix with unicode results.

--- simpleapi/server/route.py
+++ simpleapi/server/route.py
@@ -169,6 +169,9 @@ class Router(object):
             # Make call
             result = self._caller(request)
 
+            if isinstance(result['result'], unicode):
+                result['result'] = result['result'].encode("utf-8")
+
             status = '200 OK'
             headers = [('Content-type', result['mimetype'])]
             start_response(status, headers)
